6b4e07f8a1149eb0f789503c1d10d5fb9345e804
nandurty17 <nandurty17@gmail.com>
2021-05-31 00:49:16 -0700

    Create Sample Products in MongoDB

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,8 +1,18 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
+import Product from "../models/productModel.js";
 import User from "../models/userModel.js";
+import productRouter from "./productRouter.js";
 const userRouter = express.Router();
+
+productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
 
 userRouter.get(
   "/seed",
@@ -10,6 +20,18 @@ userRouter.get(
     //await User.remove({});
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
+  })
+);
+
+productRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.send(404).send({ Message: "Product not found" });
+    }
   })
 );
 
