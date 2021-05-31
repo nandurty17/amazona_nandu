f8ba2945ca368e3b00888c901523266961f5ef51
nandurty17 <nandurty17@gmail.com>
2021-05-31 00:56:33 -0700

    Adding sample users in mongo db

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -6,6 +6,14 @@ import Product from "../models/productModel.js";
 const productRouter = express.Router();
 
 productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
+productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
     //await Product.remove({});
@@ -13,4 +21,17 @@ productRouter.get(
     res.send({ createdProducts });
   })
 );
+
+productRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.send(404).send({ Message: "Product not found" });
+    }
+  })
+);
+
 export default productRouter;
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -6,32 +6,12 @@ import User from "../models/userModel.js";
 import productRouter from "./productRouter.js";
 const userRouter = express.Router();
 
-productRouter.get(
-  "/",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.send(products);
-  })
-);
-
 userRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
     //await User.remove({});
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
-  })
-);
-
-productRouter.get(
-  "/:id",
-  expressAsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.send(product);
-    } else {
-      res.send(404).send({ Message: "Product not found" });
-    }
   })
 );
 
